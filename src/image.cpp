929635310c518a431d83e8cc9c538eb8f33cf955
drusk <drusk@uvic.ca>
2014-07-08 16:54:51 -0700

    Created Image class to hold pixel values generated by the raytracer.

---
+++ src/image.cpp
@@ -0,0 +1,31 @@
+#include "image.h"
+
+Image::Image(int width, int height)
+	: width(width), height(height),
+	  pixels(new Vector3D[width * height])
+{
+}
+
+Image::~Image()
+{
+	for (int i = 0; i < width * height; i++) {
+		delete pixels[i];
+	}
+
+	delete[] pixels;
+}
+
+void Image::SetPixel(int x, int y, Vector3D colour)
+{
+	pixels[PixelIndex(x, y)] = colour;
+}
+
+Vector3D Image::GetPixel(int x, int y)
+{
+	return pixels[PixelIndex(x, y)];
+}
+
+int Image::PixelIndex(int x, int y)
+{
+	return y * width + x;
+}
---
+++ src/image.h
@@ -0,0 +1,32 @@
+#ifndef IMAGE_H_
+#define IMAGE_H_
+
+#include "vector.h"
+
+class Image
+{
+public:
+	/**
+	 * Creates an image with the specified dimensions.
+	 */
+	Image(int width, int height);
+	~Image();
+
+	void SetPixel(int x, int y, Vector3D colour);
+	Vector3D GetPixel(int x, int y);
+
+private:
+	int width;
+	int height;
+
+	/* Stored in row-major order. */
+	Vector3D *pixels;
+
+	/**
+	 * Retrieves the index in the pixel array corresponding to the
+	 * specified x and y position.
+	 */
+	int PixelIndex(int x, int y);
+};
+
+#endif
